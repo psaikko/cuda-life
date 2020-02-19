b6ec21e6da675b8fb7461bb62e4cb5fcec22ea0d
Paul Saikko <paul.saikko@gmail.com>
2020-02-19 17:17:54 +0200

    Optimize and time

--- life.cpp
+++ life.cpp
@@ -7,16 +7,16 @@
 
 using namespace std;
 
-const int W = 60;
-const int H = 20;
+const int W = 1000;
+const int H = 1000;
 
 int count_neighbors(const bool *const state, const int x, const int y) {
     int count = 0;
-    for (int xd = -1; xd <= 1; xd ++) {
-        int x_ = (x + W + xd) % W;
-        for (int yd = -1; yd <= 1; yd++) {
+    for (int yd = -1; yd <= 1; yd++) {
+        int y_ = (y + H + yd) % H;
+        for (int xd = -1; xd <= 1; xd++) {
             if (yd == 0 && xd == 0) continue;
-            int y_ = (y + H + yd) % H;
+            int x_ = (x + W + xd) % W;
             count += state[y_ * W + x_];
         }
     }
@@ -29,15 +29,9 @@ void compute_transition(const bool *const current, bool *const next) {
             int i = y * W + x;
             int n = count_neighbors(current, x, y);
             if (current[i]) {
-                if (n == 2 || n == 3)
-                    next[i] = true;
-                else 
-                    next[i] = false;
+                next[i] = (n == 2 || n == 3);
             } else {
-                if (n == 3)
-                    next[i] = true;
-                else
-                    next[i] = false;
+                next[i] = (n == 3);
             }
         }
     }
@@ -60,13 +54,15 @@ int main() {
     for (int i = 0; i < W*H; ++i)
         current[i] = rand() % 2;
 
-    for (int i = 0; i < 500; ++i) {
-        print_grid(current);
+    auto start_time = chrono::high_resolution_clock::now();
 
+    for (int i = 0; i < 100; ++i) {
         compute_transition(current, next);
-
         swap(current, next);
-        this_thread::sleep_for(chrono::milliseconds(20));
     }
-        
+
+    auto end_time = chrono::high_resolution_clock::now();
+    auto ms = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+
+    cout << ms.count() << "ms" << endl;
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 life: life.cpp
-	$(CXX) --std=c++14 -pedantic -Wall life.cpp -o life
+	$(CXX) --std=c++14 -O3 -pedantic -Wall life.cpp -o life
