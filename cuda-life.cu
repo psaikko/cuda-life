c5f3771641d731dfcc04990b507da4a5c0a2069d
Paul Saikko <paul.saikko@gmail.com>
2020-02-20 18:51:29 +0200

    Add cuda impl with one GPU thread

---
+++ cuda-life.cu
@@ -0,0 +1,71 @@
+#include <cstdlib>
+#include <cstdio>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+using namespace std;
+
+const int W = 1000;
+const int H = 1000;
+
+__global__
+void compute_transition(const bool *const current, bool *const next) { 
+    for (int y = 0; y < H; ++y) {
+        for (int x = 0; x < W; ++x) {
+            int i = y * W + x;
+            int n = 0;
+            for (int yd = -1; yd <= 1; yd++) {
+                int y_ = (y + H + yd) % H;
+                for (int xd = -1; xd <= 1; xd++) {
+                    if (yd == 0 && xd == 0) continue;
+                    int x_ = (x + W + xd) % W;
+                    n += current[y_ * W + x_];
+                }
+            }
+
+            if (current[i]) {
+                next[i] = (n == 2 || n == 3);
+            } else {
+                next[i] = (n == 3);
+            }
+        }
+    }
+}
+
+void print_grid(const bool *const grid) {
+    for (int y = 0; y < H; ++y) {
+        for (int x = 0; x < W; ++x) {
+            cout << (grid[y*W+x] ? "#" : " ");
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    bool *current, *next;
+
+    cudaMallocManaged(&current, sizeof(bool) * W * H);
+    cudaMallocManaged(&next, sizeof(bool) * W * H);
+
+    for (int i = 0; i < W*H; ++i)
+        current[i] = rand() % 2;
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < 1; ++i) {
+        compute_transition<<<1, 1>>>(current, next);
+        swap(current, next);
+    }
+
+    cudaDeviceSynchronize();
+
+    auto end_time = chrono::high_resolution_clock::now();
+    auto ms = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+
+    cout << ms.count() << "ms" << endl;
+
+    cudaFree(next);
+    cudaFree(current);
+}
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 life: life.cpp
 	$(CXX) --std=c++14 -O3 -pedantic -Wall life.cpp -o life
+
+cuda-life: cuda-life.cu
+	nvcc --std=c++14 -O3 --compiler-options -Wall cuda-life.cu -o cuda-life
